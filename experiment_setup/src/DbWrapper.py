e0c21e6459face57866459dcc223face63469bae
yotam-happy <yotamrockon2@yahoo.com>
2016-07-19 09:46:29 +0300

    DbWrapper - starting code for resolving wiki titles into db ids

--- experiment_setup/src/DbWrapper.py
+++ experiment_setup/src/DbWrapper.py
@@ -1,4 +1,4 @@
-import my0sql.connector
+import mysql.connector
 
 class WikipediaDbWrapper:
     """
@@ -21,6 +21,61 @@ class WikipediaDbWrapper:
         query = "SELECT title FROM article WHERE id = %s "
         self._cursor.execute(query, (conceptId,))
         return self._cursor.fetchone()[0].decode("utf-8")
+
+    def getPages(self):
+        query = "SELECT id, title, namespace, isRedirect FROM article"
+        self._cursor.execute(query)
+        for row in self._cursor:
+            p = Page(row[0],row[1],row[2],row[3])
+            yield p
+
+    def getRedirects(self):
+        query = "SELECT from, to, namespace FROM redirects"
+        self._cursor.execute(query)
+        for row in self._cursor:
+            yield (row[0],row[1],row[2])
+
+
+
+class Page:
+    def __init__(self, id, title, namespace, isRedirect):
+        self.id = id
+        self.title = title
+        self.isRedirect = isRedirect
+        self.namespace = namespace
+        self.redirectTo = -1
+
+class titleResolver:
+    def __init__(self, db):
+        self._pagesById = dict()
+        self._pagesByTitle = dict()
+        for page in db.getPages():
+            # we care only about main namespace
+            if page.namespace != 0:
+                continue;
+            self._pagesById[page.id] = page
+            self._pagesByTitle[page.title] = page
+
+        for red_from, red_to, red_namespace in db.getRedirects:
+            if red_namespace != 0:
+                continue
+            f = self._pagesById(red_from)
+            t = self._pagesByTitle(red_to)
+            if f is None or t is None:
+                continue
+            f.redirectTo = t.id
+
+    def resolvePage(self, title):
+        i = 0
+        while title in self._pagesByTitle and \
+                self._pagesByTitle[title].isRedirect and \
+                self._pagesByTitle[title].redirectTo >= 0 and\
+                i < 3:
+            title = self._pagesByTitle[title].title
+            i+=1
+        return self._pagesByTitle[title].id if title in self._pagesByTitle else None
+
+
 
 if __name__ == "__main__":
     wikiDB = WikipediaDbWrapper(user='root', password='???', database='wikiprep-esa-en20151002')
