20362ff70e494bd282a179289102284b860a02ae
yotam-happy <yotam.happy@gmail.com>
2016-07-26 14:08:38 +0300

    Fixed broken BaselinePairwiseModel

--- experiment_setup/src/BaselinePairwiseModel.py
+++ experiment_setup/src/BaselinePairwiseModel.py
@@ -8,7 +8,8 @@ class BaselinePairwiseModel:
         """
         :param stats: statistics object
         """
-        self._wikilink_stats = stats
+        self._stats = stats
+        self.cc = 0
 
     def predict(self, wikilink, candidate1, candidate2):
         """
@@ -17,16 +18,17 @@ class BaselinePairwiseModel:
                     This function can returns None if it has nothing to say on either of the candidates
                     this is considered as saying they are both very unlikely and should be eliminated
         """
-        if wikilink["word"] not in self._wikilink_stats.mentionLinks:
+        l = self._stats.getCandidatesForMention(wikilink["word"])
+        l = {int(x):y for x,y in l.iteritems()}
+        if l is None:
             return None
 
-        l = self._wikilink_stats.mentionLinks[wikilink["word"]]
-
         if candidate1 not in l and candidate2 not in l:
+            print l
+            print candidate1, " ", candidate2
             return None
         elif candidate1 not in l:
             return candidate2
         elif candidate2 not in l:
             return candidate1
-
         return candidate1 if l[candidate1] > l[candidate2] else candidate2
