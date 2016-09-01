21708ffd1560e4ec50711e9bfe5c4034b71ca1a4
yotam-happy <yotam.happy@gmail.com>
2016-09-01 13:57:39 +0300

    added ConllReader

---
+++ experiment_setup/src/readers/ConllReader.py
@@ -0,0 +1,89 @@
+def _CoNLLFileToDocIterator(fname, split='testb'):
+    f = open(fname,'r')
+    lines = f.readlines()
+
+    curdocName = None
+    curdocSplit = None
+    curdoc = None
+
+    for line in lines:
+        line = line.strip()
+        if line.startswith('-DOCSTART-'):
+            if curdocName is not None and (split is None or curdocSplit == split):
+                yield (curdoc, curdocName)
+            sp = line.split(' ')
+            curdocName = sp[2][:-1]
+            curdocSplit = 'testa' if sp[1].endswith('testa') else ('testb' if sp[1].endswith('testb') else 'train')
+            curdoc = []
+        else:
+            curdoc.append(line)
+    if curdocName is not None and (split is None or curdocSplit == split):
+        yield (curdoc, curdocName)
+
+
+def _CoNLLDocToSentenceIterator(docLines):
+    sent = []
+    for line in docLines:
+        if not line:
+            if len(sent) > 0:
+                yield sent
+            sent = []
+        else:
+            sent.append(line)
+
+def _CoNLLRawToTuplesIterator(lines):
+    '''
+    yields tuples:
+    (surface,ismention,islinked,YAGO2,WikiURL,WikiId,FB)
+    surface is eaither a word or the full mention
+    '''
+    for line in lines:
+        if len(line) == 0:
+            # sentence boundary.
+            continue
+        t = line.split('\t')
+        if len(t) == 1:
+            yield (t[0], False, None, None, None, None, None)
+        else:
+            if t[1] != 'B':
+                continue
+            if t[3] == '--NME--':
+                yield (t[2], True, False, None, None, None, None)
+            else:
+                yield (t[2], True, True, t[3], t[4], t[5], t[6] if len(t) >= 7 else None)
+
+def CoNLLWikilinkIterator(fname, split='testb', includeUnresolved=False):
+    for (doc,docName) in _CoNLLFileToDocIterator(fname, split):
+        for sent in _CoNLLDocToSentenceIterator(doc):
+            sent = [token for token in _CoNLLRawToTuplesIterator(sent)]
+            for i, token in enumerate(sent):
+                if token[1] and (includeUnresolved or token[2]):
+                    wlink = dict()
+                    wlink['wikiurl'] = token[4]
+                    wlink['yago2'] = token[3]
+                    wlink['wikiId'] = token[5] # BE CAREFUL!! This might be a different mapping then ours
+                    wlink['word'] = token[0]
+
+                    left_context_text = ' '.join([x[0] for x in sent[:i]])
+                    right_context_text = ' '.join([x[0] for x in sent[i+1:]])
+                    wlink['left_context_text'] = left_context_text
+                    wlink['right_context_text'] = right_context_text
+                    wlink['left_context'] = left_context_text.split(' ')
+                    wlink['right_context'] = right_context_text.split(' ')
+
+                    yield wlink
+
+if __name__ == "__main__":
+    from WikilinksStatistics import *
+    stats = WikilinksStatistics(None, "../data/wikilinks/train-stats")
+
+    total = 0
+    mentions_exist = 0
+    for i, wlink in enumerate(CoNLLWikilinkIterator('../data/CoNLL/CoNLL_AIDA-YAGO2-dataset.tsv')):
+        cands = stats.getCandidatesForMention(wlink['word'])
+        total += 1
+        if cands is not None:
+            mentions_exist += 1
+        else:
+            print "not found: ", wlink['word']
+    print "mentions found: ", mentions_exist, "out of ", total, "(", float(mentions_exist) / total, "%)"
