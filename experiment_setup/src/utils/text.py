6258aa90855874774c806734c98cea8168905738
yotam-happy <yotam.happy@gmail.com>
2016-10-10 13:13:29 +0300

    added strip_wiki_title to text module. This module should contain basic and sensitive string/unicode processing operations

--- experiment_setup/src/utils/text.py
+++ experiment_setup/src/utils/text.py
@@ -1,7 +1,18 @@
 import unicodedata
+import re
+
+
 def normalize_unicode(str):
-    print str
     s = str.decode("utf-8", 'ignore').lower()
     s = unicodedata.normalize('NFKD', s)
     s = s.encode('ascii', 'ignore')
     return s
+
+
+def strip_wiki_title(title):
+    t = re.sub('[^0-9a-zA-Z]+', '_', normalize_unicode(title))
+    if len(t) > 1 and t[0] == '_':
+        t = t[1:]
+    if len(t) > 1 and t[-1] == '_':
+        t = t[:-1]
+    return t
