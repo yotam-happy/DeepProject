8e2dcfa5b5558a75bcdaff46f0400bc61d5445f5
yotam-happy <yotam.happy@gmail.com>
2016-10-10 13:14:50 +0300

    Removed caching mechanism. Added not-yet-tested new resolve mechanism that should be more robust.

--- experiment_setup/src/DbWrapper.py
+++ experiment_setup/src/DbWrapper.py
@@ -1,27 +1,20 @@
 import mysql.connector
 import re
 import urllib
-
+import utils.text
 class WikipediaDbWrapper:
     """
     Some initial efforts at supporting Db for the project
     Note this class is not thread safe or anything so be aware...
     """
 
-    def __init__(self, user, password, database, host='127.0.0.1', cache=False):
+    def __init__(self, user, password, database, host='127.0.0.1'):
         """
         All the parameters are self explanatory...
         """
 
         self._cnx = mysql.connector.connect(user=user, password=password, host=host, database=database)
         self._cursor = self._cnx.cursor(buffered=True)
-        self._pageInfoByIdCache = None
-        self._pageInfoByTitleCache = None
-        self._articleId = None
-        self._articleTitle = None
-        if cache:
-            self.cachePageInfoTable()
-            self.cacheArticleTable()
         self._articleInlinks = None
 
         self.cc = 0
@@ -33,63 +26,46 @@ class WikipediaDbWrapper:
         """
         Gets the wikipedia page title for a given id.
         """
-        if self._articleId is not None:
-            return self._articleId[conceptId] if conceptId in self._articleId else None
-        else:
-            query = "SELECT title_resolver FROM article where id=%s"
-            self._cursor.execute(query, (conceptId,))
-            row = self._cursor.fetchone()
-            if row == None:
-                return None
-            return row[0]
+        query = "SELECT title_resolver FROM article where id=%s"
+        self._cursor.execute(query, (conceptId,))
+        row = self._cursor.fetchone()
+        if row == None:
+            return None
+        return row[0]
 
     def getArticleIdByTitle(self, title):
         """
         Gets the wikipedia page title for a given id.
         """
-        title = self.stripTitle(title)
-        if self._articleTitle is not None:
-            return self._articleTitle[title] if title in self._articleTitle else None
-        else:
-            query = "SELECT id FROM article where title_resolver=%s"
-            self._cursor.execute(query, (title,))
-            row = self._cursor.fetchone()
-            if row == None:
-                return None
-            return int(row[0])
+        query = "SELECT id FROM article where title_resolver=%s"
+        self._cursor.execute(query, (title,))
+        row = self._cursor.fetchone()
+        if row == None:
+            return None
+        return int(row[0])
 
-    def printSomeArticle(self):
-        query = "SELECT title, id FROM article"
-        self._cursor.execute(query)
+    def updatePageTableTitleForLookupColumn(self):
+        self._cnx.autocommit = False
+        query = "SELECT page_title, page_id FROM page"
         i = 0
+        fetch_cursor = self._cnx.cursor(buffered=True)
+        fetch_cursor.execute(query)
+        print 'updating'
         while True:
-            row = self._cursor.fetchone()
+            row = fetch_cursor.fetchone()
             if not row:
                 break
-            t = row[0].decode("utf-8")
-            print row[1], ": ", self.stripTitle(t)
-            if i > 100:
-                return
-            i += 1
+            t = utils.text.strip_wiki_title(row[0])
+            self._cursor.execute("""
+            UPDATE page SET page_title_for_lookup=%s WHERE page_id=%s
+            """, (t, int(row[1])))
 
-    def cacheArticleTable(self):
-        query = "SELECT title, id FROM article"
-        self._articleId = dict()
-        self._articleTitle = dict()
-
-        i = 0
-        self._cursor.execute(query)
-        print "caching"
-        while True:
-            row = self._cursor.fetchone()
-            if not row:
-                break
-            t = self.stripTitle(row[0].decode("utf-8"))
-            self._articleId[row[1]] = t
-            self._articleTitle[t] = row[1]
-            if i % 100000 == 0:
-                print "caching article ", i
             i += 1
+            if i % 10000 == 0:
+                print "updated ", i
+            if i % 1000000 == 0:
+                self._cnx.commit()
+
 
     def updateTables(self):
         print "self._cnx.autocommit:", self._cnx.autocommit
@@ -116,62 +92,15 @@ class WikipediaDbWrapper:
                 print "done ", n
         self._cnx.commit()
 
-    def cacheArticleInlinksTable(self):
-        query = "SELECT target_id, inlink FROM inlinks"
-        self._articleInlinks = dict()
-
-        i = 0
-        self._cursor.execute(query)
-        print "caching"
-        while True:
-            row = self._cursor.fetchone()
-            if not row:
-                break
-            self._articleInlinks[row[0]] = row[1]
-            if i % 100000 == 0:
-                print "caching article ", i
-            i += 1
-
-    def getInlinks(self, wikiId):
-        if self._articleInlinks is None:
-            raise "Must cache table!"
-        return self._articleInlinks[wikiId] if wikiId in self._articleInlinks else None
-
-    def cachePageInfoTable(self):
-        query = "SELECT page_id, namespace, title, redirect FROM pages_redirects where namespace=0"
-        self._pageInfoByTitleCache = dict()
-        self._pageInfoByIdCache = dict()
-        self._cursor.execute(query)
-        i = 0
-        while True:
-            row = self._cursor.fetchone()
-            if not row:
-                break
-            t = row[2].decode("utf-8")
-            self._pageInfoByTitleCache[t] = (row[0], row[1], t, row[3])
-            self._pageInfoByIdCache[row[0]] = (row[0], row[1], t, row[3])
-            if i % 100000 == 0:
-                print "caching redirect ", i
-            i += 1
-        print "cached ", len(self._pageInfoByTitleCache), " entries"
-
     def getPageInfoByTitle(self, title):
-        if self._pageInfoByTitleCache is not None:
-            return self._pageInfoByTitleCache[self.stripTitle(title)] \
-                if self.stripTitle(title) in self._pageInfoByTitleCache \
-                else (None,None,None,None)
         query = "SELECT page_id, namespace, title, redirect FROM pages_redirects WHERE title = %s"
-        self._cursor.execute(query, (self.stripTitle(title),))
+        self._cursor.execute(query, (utils.text.strip_wiki_title(title),))
         row = self._cursor.fetchone()
         if row == None:
             return (None,None,None,None)
         return (row[0], row[1], row[2].decode("utf-8"), row[3])
 
     def getPageInfoById(self, page_id):
-        if self._pageInfoByIdCache is not None:
-            return self._pageInfoByIdCache[page_id] \
-                if page_id in self._pageInfoByIdCache \
-                else (None,None,None,None)
         query = "SELECT page_id, namespace, title, redirect FROM pages_redirects WHERE page_id = %s"
         self._cursor.execute(query, (page_id,))
         row = self._cursor.fetchone()
@@ -179,9 +108,78 @@ class WikipediaDbWrapper:
             return (None,None,None,None)
         return (row[0], row[1], row[2].decode("utf-8"), row[3])
 
-    def stripTitle(self,title):
-        t = re.sub('[^0-9a-zA-Z]', '_', title.lower())
-        return t
+    def resolvePage2(self, title, verbose=False, use_pagelink_table=False):
+        '''
+        Resolving a page id.
+        We first use utils.text.strip_wiki_title to compute a cleaned title
+        We then query the page table.
+        If this is indicated as a redirect page then we query the redirect table
+        If the redirect does not appear in the redirect table then we try the pagelink table as older
+        redirects might be found there.
+        We take into account that a chain of redirects might be encountered
+        If the page at the end of the chain is still redirect we return None
+        :param title:               title of page
+        :param verbose:             true to show the steps along the way
+        :param use_pagelink_table: if true we try the pagelink table as described above
+        :return:                    id of page or None if we couldn't resolve
+        '''
+        if verbose:
+            print "resolving title: ", title
+
+        # sometimes the titles come with url type quating (e.g 'the%20offspring')
+        title = urllib.unquote(title)
+        title = utils.text.strip_wiki_title(title)
+        if verbose:
+            print "lookup key: ", title
+
+        # get page
+        query = "SELECT page_id, page_is_redirect, page_title FROM page " \
+                "WHERE page_title_for_lookup = %s and page_namespace = 0"
+        self._cursor.execute(query, (title,))
+        row = self._cursor.fetchone()
+        if row is None:
+            if verbose:
+                print "could not find page (in main namespace)"
+            return None
+
+        page_id = int(row[0])
+        page_red = int(row[1])
+        page_title = row[2]
+        if verbose:
+            print "got page id =", page_id, "; title =", page_title, "; redirect =", page_red
+
+        while page_red == 1:
+            # query next page using redirect table
+            query = "SELECT page_id, page_is_redirect, page_title FROM page " \
+                    "WHERE page_namespace = 0 AND page_title IN " \
+                    "(SELECT rd_title FROM redirect WHERE rd_namespace = 0 AND rd_from = $s)"
+            self._cursor.execute(query, (page_id,))
+            row = self._cursor.fetchone()
+
+            if row is None and use_pagelink_table:
+                if verbose:
+                    print "redirect not found in redirect table, trying pagelink..."
+                # try using pagelink (some older redirects can only be found here)
+                query = "SELECT page_id, page_is_redirect, page_title FROM page " \
+                        "WHERE page_namespace = 0 AND page_title IN " \
+                        "(SELECT pl_title FROM pagelink WHERE pl_namespace = 0 AND pl_from = $s)"
+                self._cursor.execute(query, (page_id,))
+                row = self._cursor.fetchone()
+
+            if row is None:
+                if verbose:
+                    print "could not resolve redirect"
+                return None
+
+            page_id = int(row[0])
+            page_red = int(row[1])
+            page_title = row[2]
+            if verbose:
+                print "got page id =", page_id, "; title =", page_title, "; redirect =", page_red
+
+        if verbose:
+            print "return", page_id
+        return page_id
 
     def resolvePage(self, title, verbose=False):
         '''
@@ -220,33 +218,6 @@ class WikipediaDbWrapper:
             i+=1
         return candidate
 
-
-    def resolvePage2(self, title):
-        '''
-        Not trivial.
-        '''
-        title = urllib.unquote(title)
-        candidate = self.getArticleIdByTitle(title)
-        page_id, namespace, title, redirect = self.getPageInfoByTitle(title)
-        if self.getArticleTitleById(page_id) is not None:
-            candidate = page_id
-
-        i = 0
-        while page_id is not None and \
-                        redirect > -1 and \
-                        i < 3:
-            page_id, namespace, title, redirect = self.getPageInfoById(redirect)
-            if page_id is not None and self.getArticleTitleById(page_id) is not None:
-                candidate = page_id
-            elif title is not None:
-                x = self.getArticleIdByTitle(title)
-                if x is not None:
-                    candidate = x
-            i += 1
-        return candidate
-
-
-#if __name__ == "__main__":
-#    wikiDB = WikipediaDbWrapper(user='yotam', password='rockon123', database='wiki20151002')
-#    wikiDB.cacheArticleTable()
-#    wikiDB.printSomeArticle()
+if __name__ == "__main__":
+    wikiDB = WikipediaDbWrapper(user='yotam', password='rockon123', database='wiki20151002')
+    wikiDB.updatePageTableTitleForLookupColumn()
